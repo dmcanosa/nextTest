f3ac980b29ab3d7da2e6524470955274b31f881c
Daniel <dc@tomorrow.media>
2025-02-17 22:55:33 -0300

    refactoring

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,7 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
+//import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -15,17 +15,17 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const {
+  /*const {
     numberOfInvoices,
     numberOfCustomers,
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
-
+  */
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
-
+      {/*
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -34,6 +34,7 @@ export default async function CardWrapper() {
         value={numberOfCustomers}
         type="customers"
       /> 
+      */}
     </>
   );
 }
