b60a3dc848d0dd0d99c96c7f113f855ce8de895c
Daniel <dc@tomorrow.media>
2025-02-28 04:49:52 -0300

    docs added

--- app/ui/documents/create-form.tsx
+++ app/ui/documents/create-form.tsx
@@ -20,15 +20,18 @@ export default function Form(/*{ customers }: { customers: CustomerField[] }*/) 
         {/* Signature */}
         <div className="mb-4">
           <label htmlFor="signature" className="mb-2 block text-sm font-medium">
-            Draw your signature
+            Upload the template document
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
-              
-              <input type='hidden' name='canvasString' id='canvasString'></input>
+              <p className='template_name' id='template_name'>
+
+              </p>
+              <input type='file' name='template_file' id='template_file'></input>
+              <input type='hidden' name='signature_id' id='signature_id'></input> 
             </div>
           </div>
-          <div id="canvas-error" aria-live="polite" aria-atomic="true">
+          <div id="template-error" aria-live="polite" aria-atomic="true">
             {state.errors?.template_name &&
               state.errors.template_name.map((error: string) => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
@@ -43,12 +46,12 @@ export default function Form(/*{ customers }: { customers: CustomerField[] }*/) 
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
-          href="/dashboard/signatures"
+          href="/dashboard/documents"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
         </Link>
-        <Button type="submit">Create Signature</Button>
+        <Button type="submit">Create Document</Button>
       </div>
     </form>
   );
