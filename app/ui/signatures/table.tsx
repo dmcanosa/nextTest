3927d0d0df63e510465db78cc15d7fc1272f14b4
Daniel <dc@tomorrow.media>
2025-03-12 00:36:32 -0300

    fixing key error

--- app/ui/signatures/table.tsx
+++ app/ui/signatures/table.tsx
@@ -33,7 +33,7 @@ export default async function SignaturesTable({
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-          <div className="md:hidden">
+          <div className="md:hidden" key="key" >
             {decryptedSignatures?.map((signature) => (
               <div
                 key={signature.id}
