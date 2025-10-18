b679bdc5b176ce5dedf787b8dc7bde24be844d2b
Daniel <dc@tomorrow.media>
2025-10-18 00:46:04 -0300

    removing api

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,45 +1,12 @@
 import { lusitana } from '@/app/ui/fonts';
-//import { fetchCardData } from '@/app/lib/data';
 
 export default async function Page() {
-  //const revenue = await fetchRevenue();
-  //const latestSignatures = await fetchLatestSignatures();
-  /*const { 
-    numberOfCustomers,
-    numberOfSignatures,
-    totalPaidSignatures,
-    totalPendingSignatures } = await fetchCardData();*/
-
+  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      
-      {/*<div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
-        
-        {/*<Card title="Collected" value={totalPaidSignatures} type="collected" /> }
-        {<Card title="Pending" value={totalPendingSignatures} type="pending" /> }
-        {<Card title="Total Signatures" value={numberOfSignatures} type="Signatures" /> }
-        {<Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> 
-        }
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-         <Suspense fallback={ <RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
-        <Suspense fallback={ <LatestSignaturesSkeleton />}>
-          <LatestSignatures />
-        </Suspense> 
-          
-      </div>*/}
     </main>
   );
 }
